6c0e22f1573835c1a6b3d60eda59ae2267ba4c58
roman_notebook <sindeevr5@gmail.com>
2024-12-02 22:57:00 +0300

    Сервер и определение ручек

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,31 @@
+from email import message_from_string
+from pyexpat.errors import messages
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import asyncio
+from LpuService import LpuService
 
 app = FastAPI()
 
-@app.get("/")
-def test():
-    return None
+@app.get("/lpu")
+def get_lpu_list():
+    service = LpuService()
+    data = service.get_names()
+    if data:
+        return {"names": data}
+    raise HTTPException(status_code=404)
+
+@app.get("/lpu/{name}")
+def get_lpu_data(host):
+    if not host:
+        raise HTTPException(status_code=400)
+    service = LpuService()
+    data = service.get_lpu_data(host)
+    if data:
+        return {"data": data}
+    raise HTTPException(status_code=400)
+
 
 if __name__ == "__main__":
     uvicorn.run(
